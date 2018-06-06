39358291c7823ba4779b0382811c407428b7fc7b
Simon Zlejsi <simonzlejsi1@gmail.com>
2018-06-06 15:48:05 +0200

    minor upgrades and creating README

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
 # Memory Game Project
 
+- this is my udacity student project
+
 ## Table of Contents
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+* [Instructions] the game starts and you have to find matching cards, there are 16 cards and 8 pairs. Find the matching pair to WIN the game. good luck and have fun
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+There is no possibility of contribution to this project. Project is private.
 
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+## EXTERNAL LIBRARIES 
+
+- google fonts - https://fonts.googleapis.com/css?family=Coda
+- fontawesome - https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css
+- JS TIMER FUNCTION BY HUSA - https://github.com/husa/timer.js/blob/master/README.md
+- Shuffle function - http://stackoverflow.com/a/2450976
--- js/app.js
+++ js/app.js
@@ -82,23 +82,68 @@ function movesN() {
 		case 16:
 			document.querySelector('.stars').firstElementChild.remove();
 			break;
-		case 18: 
-			document.querySelector('.stars').firstElementChild.remove();
-			break;
-		case 20:
+		case 20: 
 			document.querySelector('.stars').firstElementChild.remove();
 			break;
 		default:
 	}
 }
 
+// WINNING THE GAME FUNCTION 
+
+function openModal() {
+	const popUp = document.querySelector('#modal');
+	popUp.style.display = 'flex';
+	myTimer.measureStop('label1');
+	// TIME
+
+	var elapsed = myTimer.measureStop('label1');
+	document.querySelector('.time').textContent = 'your time  '+ elapsed/1000 + 'seconds';
+	// ADD STARS
+	const stars = document.getElementsByClassName('fa-star');
+	const starsCon = document.querySelector('.stars');
+
+	const arStars = Array.from(stars);
+	document.querySelector('.stars-count').textContent = 'number of stars -->  ' + arStars.length;
+	// BUTTONS
+	const close = document.querySelector('.exit');
+	const again = document.querySelector('.restart');
+
+	close.addEventListener('click',function () {
+		popUp.style.display = 'none';
+	});
+
+	again.addEventListener('click',function () {
+		getClasses();
+		shuffle(cardsList);
+		setClasses();
+
+		counter= 0;
+		moves = 0 ;
+		document.querySelector('.moves').innerHTML = moves ;
+		theList = [];
+		for (let i = 0; i < 16; i++) {
+			cards[i].classList.remove('show','open','match');
+		}
+
+		deck.addEventListener('click', timerF);
+		starReset();
+
+		popUp.style.display = 'none';
+	});
+}
+
+
+
+
 var theList = [];
 let counter = 0 ;
+let win = 0 ;
+
+// MATCH VALIDATION FUNCTION
 
 function validation() {
-	if (theList[0].parentElement === theList[1].parentElement/*theList[0].className == theList[1].className*/) {
-		// theList[0].parentElement.classList.add('match');
-		// theList[1].parentElement.classList.add('match');
+	if (theList[0].parentElement === theList[1].parentElement) {
 		let firstE = theList[0];
 		let secondE = theList[1];
 
@@ -108,9 +153,16 @@ function validation() {
 		}
 
 		 setTimeout(ifFalse, 1000);
-	} else if (theList[0].className == theList[1].className/*theList[0].parentElement === theList[1].parentElement*/){
+	} else if (theList[0].className == theList[1].className){
 		theList[0].parentElement.classList.add('match');
 		theList[1].parentElement.classList.add('match');
+		win += 2 ;
+		// WINNING THE GAME 
+			if (win == 16) {
+				openModal();
+			}
+
+
 		}
 	else {
 		let firstE = theList[0];
@@ -123,9 +175,12 @@ function validation() {
 
 		 setTimeout(ifFalse, 1000);
 	}
+
 }
 
-deck.addEventListener('click',function (event) {
+// EVENT LISTENER ON CLICK
+
+function listener(event) {
 	theCard = event.target;
 	// targeting card
 	
@@ -147,9 +202,20 @@ deck.addEventListener('click',function (event) {
 		movesN();
 
 	}
-	}
 
-});
+	}
+}
+
+function timerF() {
+	myTimer.measureStart('label1');
+	deck.removeEventListener('click', timerF);
+}
+
+deck.addEventListener('click', listener);
+deck.addEventListener('click', timerF);
+
+// EVENT LISTENER ON CLICK
+// RESET BUTTON FUNCTIONS
 
 function starReset() {
 	const stars = document.getElementsByClassName('fa-star');
@@ -171,7 +237,7 @@ reset.addEventListener('click', function () {
 	getClasses();
 	shuffle(cardsList);
 	setClasses();
-	
+
 	counter= 0;
 	moves = 0 ;
 	document.querySelector('.moves').innerHTML = moves ;
@@ -180,6 +246,15 @@ reset.addEventListener('click', function () {
 		cards[i].classList.remove('show','open','match');
 	}
 
-
+	deck.addEventListener('click', timerF);
 	starReset();
 });
+
+
+// JS TIMER FUNCTION BY HUSA - https://github.com/husa/timer.js/blob/master/README.md
+
+!function(a,b){"use strict";"function"==typeof define&&define.amd?define([],b):"object"==typeof exports?module.exports=b():a.Timer=b()}(this,function(){"use strict";function a(){c.call(this),this._.status="stopped",b.call(this,"onend")}function b(a){var b=this._.options[a],c=[].slice.call(arguments,1);"function"==typeof b&&b.apply(this,c)}function c(a){clearTimeout(this._.timeout),clearInterval(this._.interval),a===!0&&(this._.duration=0)}var d={tick:1,onstart:null,ontick:null,onpause:null,onstop:null,onend:null},e=function(a){if(!(this instanceof e))return new e(a);this._={id:+new Date,options:{},duration:0,status:"initialized",start:0,measures:[]};for(var b in d)this._.options[b]=d[b];this.options(a)};return e.prototype.start=function(c){return+c||this._.duration?(c&&(c*=1e3),this._.timeout&&"started"===this._.status?this:(this._.duration=c||this._.duration,this._.timeout=setTimeout(a.bind(this),this._.duration),"function"==typeof this._.options.ontick&&(this._.interval=setInterval(function(){b.call(this,"ontick",this.getDuration())}.bind(this),1e3*+this._.options.tick)),this._.start=+new Date,this._.status="started",b.call(this,"onstart",this.getDuration()),this)):this},e.prototype.pause=function(){return"started"!==this._.status?this:(this._.duration-=+new Date-this._.start,c.call(this,!1),this._.status="paused",b.call(this,"onpause"),this)},e.prototype.stop=function(){return/started|paused/.test(this._.status)?(c.call(this,!0),this._.status="stopped",b.call(this,"onstop"),this):this},e.prototype.getDuration=function(){return"started"===this._.status?this._.duration-(+new Date-this._.start):"paused"===this._.status?this._.duration:0},e.prototype.getStatus=function(){return this._.status},e.prototype.options=function(a,b){if(a&&b&&(this._.options[a]=b),!b&&"object"==typeof a)for(var c in a)this._.options.hasOwnProperty(c)&&(this._.options[c]=a[c]);return this},e.prototype.on=function(a,b){return"string"!=typeof a||"function"!=typeof b?this:(/^on/.test(a)||(a="on"+a),this._.options.hasOwnProperty(a)&&(this._.options[a]=b),this)},e.prototype.off=function(a){return"string"!=typeof a?this:(a=a.toLowerCase(),"all"===a?(this._.options=d,this):(/^on/.test(a)||(a="on"+a),this._.options.hasOwnProperty(a)&&(this._.options[a]=d[a]),this))},e.prototype.measureStart=function(a){return this._.measures[a||""]=+new Date,this},e.prototype.measureStop=function(a){return+new Date-this._.measures[a||""]},e});	
+
+// TIMER INITIALIZATION
+
+var myTimer = new Timer();
